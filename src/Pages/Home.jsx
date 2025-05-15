fb8f3623d0cf58438845a1e60d2a032bfb8fa82f
jatingrg <jatingarg94786@gmail.com>
2025-05-15 16:46:03 +0530

    updatingthefiles

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
     <>
-    <div className='container'>
-      <div className='home_container'>
-        <p className='home_container_para'>Hi all. I m</p>
-        <h1 className='home_container_heading'>Jatin Garg</h1>
-        <h4 className='home_container_heading4'>Frontend Developer</h4>
-        <span className='home_container_profilegit'>//Find my profile on github</span>
-        <p className="home_container_git">
-  <span className="home_container_git1">const</span>
-  <span className="home_container_git2">githubLink</span>
-  <span className="home_container_git3">=</span>
-</p>
-    </div>
-    <div>
-     <div className="shape shape1"></div>
-<div className ="shape shape2"></div>
+     <div className='container'>
+  <div className='home_container'>
+    <p className='home_container_para'>Hi all. I am</p>
+    <h1 className='home_container_heading'>Jatin Garg</h1>
+    <h4 className='home_container_heading4'>Front-end Developer</h4>
+    <span className='home_container_profilegit'>// Find my profile on GitHub</span>
+    <p className="home_container_git">
+      <span className="home_container_git1">const</span>{' '}
+      <span className="home_container_git2">githubLink</span>{' '}
+      <span className="home_container_git3">=</span>{' '}
+      <span className="home_container_git4">
+        <a href="https://github.com/jatingrg" target="_blank" rel="noreferrer">
+          "https://github.com/jatingrg"
+        </a>
+      </span>
+    </p>
+  </div>
 
-    </div>
-    </div>
-  
+  <div className="home_right_wrapper">
+    <img src="/profilepic.png" alt="Profile" className="home_profilepic" />
+    <div className="shape shape1"></div>
+    <div className="shape shape2"></div>
+  </div>
+</div>
+
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
